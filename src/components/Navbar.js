68214866826a086513f20092d0adc605b67d6d6e
Ayush Shah <shahayush934@gmail.com>
2022-05-11 15:57:31 +0530

    First Commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+const Navbar = () => {
+    return ( 
+        <nav class="navbar navbar-expand-lg navbar-dark navbar-component">
+          <a class="navbar-brand" href="/">Travel Buddy</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="navbar navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto">
+              <li class="nav-item text-dark nav-item-style">
+                <a class="nav-link" href="/">SignUP</a>
+              </li>
+              <li class="nav-item text-dark nav-item-style">
+                <a class="nav-link" href="/">Login</a>
+              </li>
+              <li class="nav-item text-dark nav-item-style">
+                <a class="nav-link" href="/"><i className="fa-solid fa-user"></i></a>
+              </li>
+            </ul>
+          </div>
+      </nav>
+    );
+}
+export default Navbar;
